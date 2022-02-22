787be840487871ab0db0adc116bad69683353200
Clay <claymcintosh@gmail.com>
2022-02-23 09:09:29 +1000

    Set up express with port

--- server.js
+++ server.js
@@ -1,1 +1,4 @@
-const express = require("express");
+const app = require("express")();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`running on http://localhost:${PORT}`));
