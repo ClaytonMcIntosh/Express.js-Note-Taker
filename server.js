f0a762073e28cc340e6cfd22ab92a9f6a228ed65
Clay <claymcintosh@gmail.com>
2022-02-23 09:35:45 +1000

    running simple test on express and insomnia

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.listen(PORT, () => console.log(`running on http://localhost:${PORT}`));
+
+app.get("/notes", (req, res) => {
+  res.status(200).send({
+    key: "value",
+    key2: "value2",
+  });
+});
+
+app.post("/notes/:id", (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(418).send({ message: "we need a logo" });
+  }
+
+  res.send({
+    notes: `Tshirt with your ${logo} and ID of ${id}`,
+  });
+});
