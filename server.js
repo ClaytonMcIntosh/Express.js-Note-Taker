c4175eb1fe3a5cd3d4671f6e7425c8a3b191e5b3
Clay <claymcintosh@gmail.com>
2022-02-23 11:38:17 +1000

    fixed file name problems

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./public/"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
