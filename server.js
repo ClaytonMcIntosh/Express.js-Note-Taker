8245500cc7e2b05e9e0a7da19487a35f2a2c8f19
Clay <claymcintosh@gmail.com>
2022-02-23 15:04:02 +1000

    Able to get the note taker to respond but have some issues to work out with posting.

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
+//only needed for posting
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -15,6 +16,26 @@ app.get("/notes", (req, res) => {
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.post("/api/notes", (req, res) => {
+  let noteReq = req.body;
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteId = notes.length++;
+  const newNote = {
+    id: noteId,
+    title: noteReq.title,
+    text: noteReq.text,c
+  };
+
+  notes.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+  res.json(noteList);
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
