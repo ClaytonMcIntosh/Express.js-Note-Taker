8a1545843f28ff2905c7f96e201c50cf0eeae20c
Clay <claymcintosh@gmail.com>
2022-02-24 17:03:25 +1000

    Rearranged the code

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
-app.get("/api/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/db/db.json"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.post("/api/notes", (req, res) => {
@@ -31,8 +31,6 @@ app.post("/api/notes", (req, res) => {
     text: noteReq.text,
   };
   notes.push(newNote);
-
-
 
   fs.writeFileSync("./db/db.json", JSON.stringify(notes));
   res.json(notes);
